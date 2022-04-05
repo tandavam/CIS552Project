cb04e4b8ef48fb775b60cbaf429a7aeabdc18950
tandavam <sachinsrini5@gmail.com>
2022-04-05 08:34:26 -0400

    parse query string

--- src/com/dbDesign/Main.java
+++ src/com/dbDesign/Main.java
@@ -1,44 +1,43 @@
 package com.dbDesign;
 import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.ParseException;
 import net.sf.jsqlparser.statement.Statement;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String str;
+        File sqlQueryFile = new File(args[0]);
+        GlobalVariables.collection_location = new File(args[1]);
+        FileInputStream file_pointer = new FileInputStream(sqlQueryFile);
         try {
-            File sqlQueryFile = new File(args[0]);
-            GlobalVariables.collection_location = new File(args[1]);
-            FileInputStream file_pointer = new FileInputStream(sqlQueryFile);
             byte[] data = new byte[(int) sqlQueryFile.length()];
             file_pointer.read(data);
             file_pointer.close();
             str = new String(data, StandardCharsets.UTF_8);
             String[] list_of_queries = str.split(";");
-            for (String query : list_of_queries) {
-                if (!query.strip().equals("")) {
-                    Reader input = new StringReader(query);
-                    CCJSqlParser parser = new CCJSqlParser(input);
-                    try {
-                        Statement statement = parser.Statement();
-                        statement.accept(new QueryParser());
-                    } catch (Exception e) {
-                        System.out.println("SQL syntax error");
-                        e.printStackTrace();
-                    }
+            for(int query = 0; query < list_of_queries.length - 1; query++){
+                if (!list_of_queries[query].strip().equals("")) {
+                    parse_query_string(list_of_queries[query]);
                 }
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
+    }
+
+    private static void parse_query_string(String list_of_queries) throws ParseException {
+        Reader input = new StringReader(list_of_queries);
+        CCJSqlParser parser = new CCJSqlParser(input);
+        Statement statement = parser.Statement();
+        statement.accept(new QueryParser());
     }
 }
